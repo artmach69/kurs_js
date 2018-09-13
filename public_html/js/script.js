44d52c5b57c67689223f6a59dcbe6390a2c04282
artmach69 <artmach@wp.pl>
2018-09-13 11:34:14 +0200

    zmiana

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -5,15 +5,17 @@
  */
 
 
-var kursyProgramowania = [
-    "C++",
-    "Java",
-    "C#",
-    "Pascal"
-];
 
-for (var i = 0; i<5; i++)
 
-{
-  document.getElementById("rezultat").innerHTML += i + "<br>";
-}
+  var kursyProgramowania = document.getElementById("kursyProgramowania").getElementsByTagName("li");
+  
+  for (var i=0; i < kursyProgramowania.length; i++)
+  {
+    if (i % 2 !==0)
+    kursyProgramowania[i].innerHTML = "parzysty: " + kursyProgramowania[i].innerHTML;
+else
+    continue;
+
+
+alert(i);
+  }
