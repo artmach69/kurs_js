770fcc1a4610a176df29f937cbe64e2335f85d8f
artmach69 <artmach@wp.pl>
2018-09-18 14:13:29 +0200

    listener

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,4 +4,32 @@
  * and open the template in the editor.
  */
 
+function zmienKolor()
+{
+    this.className = "zmienKolor";
+}
+
+function zmienKolor2()
+{
+    this.className = "zmienKolor2";
+}
+
+function powiekszCzcionke ()
+{
+ 
+    var fontSize = parseInt(window.getComputedStyle(this).fontSize);
+    this.style.fontSize = (++fontSize) + "px";
+}
+
+window.onload = function ()
+
+{
+var test = document.querySelector("#test");
+    //test.onmouseover = zmienKolor;
+    //test.onmouseout = zmienKolor2;
+    
+    test.addEventListener("mouseover", zmienKolor);
+    test.addEventListener("mouseover", powiekszCzcionke);
+   
+};
 
