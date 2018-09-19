3e47a2bc67f5de5f7aa5fcf1a4b1618b7c496629
Artur Machowski <artmach@wp.pl>
2018-09-19 11:44:08 +0200

    object

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,49 +4,17 @@
  * and open the template in the editor.
  */
 
-function createEvent (obj, eventName, functionToInvoke)
+function wykonaj(event)
 {
-    if (document.addEventListener)
-        obj.addEventListener(eventName, functionToInvoke);
-    else
-        obj.attachEvent("on", eventName, functionToInvoke);
-}
-
-function zmienKolor()
-{
-    this.className = "zmienKolor";
-}
-
-function zmienKolor2()
-{
-    this.className = "zmienKolor2";
-}
-
-function powiekszCzcionke ()
-{
- 
-    var fontSize = parseInt(window.getComputedStyle(this).fontSize);
-    this.style.fontSize = (++fontSize) + "px";
+    var tmp = document.querySelector("#tmp");
+    tmp.innerHTML = event;
 }
 
 window.onload = function ()
 
 {
 var test = document.querySelector("#test");
-var stop = document.querySelector("#stop");
-  
-    createEvent(test, "mouseover", zmienKolor);
-    createEvent(test, "mouseover", powiekszCzcionke);
-    
-    
-    //test.addEventListener("mouseover", zmienKolor);
-    //test.addEventListener("mouseover", powiekszCzcionke);
-    
-    
-    stop.addEventListener("click", function ()
-    {
-    test.removeEventListener("mouseover", powiekszCzcionke);    
-    });
-   
+
+test.onmousemove = wykonaj;
 };
 
