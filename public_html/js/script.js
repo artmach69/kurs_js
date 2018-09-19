38620ec25a5c20de0ba839484c64c18b9e311750
Artur Machowski <artmach@wp.pl>
2018-09-19 20:07:05 +0200

    scroll

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -7,19 +7,11 @@
 window.onload = function ()
 
 {
-var email = document.querySelector("#email");
-
-var submitFormButton = document.querySelector("#newsletter input [type = 'submit']");
-
-submitFormButton.onclick = function(e)
-{
-  var tmp = document.querySelector("#tmp");
-  e.preventDefault();
-    tmp.innerHTML = email.value;  
-    
-    if (email.value === 'artmach@wp.pl')
-    this.parentNode.submit();
-};
-
+ var toTopButton = document.querySelector("#toTopButton");
+ 
+ toTopButton.onclick = function()
+ {
+    window.scrollBy(0, 100); 
+ };
 };
 
