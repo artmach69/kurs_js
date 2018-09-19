32835e14d7a7578dfa5e816e29e9912a52241fe1
Artur Machowski <artmach@wp.pl>
2018-09-19 20:34:24 +0200

    scroll_zmiana

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,9 +9,25 @@ window.onload = function ()
 {
  var toTopButton = document.querySelector("#toTopButton");
  
+ 
+    window.onscroll = function()
+    {
+       var test = document.querySelector("#test");
+       var toTopButton = document.querySelector("#toTopButton");
+       
+       var yScrollAxis = window.pageYOffset;
+       
+        if (yScrollAxis > 300)
+            toTopButton.style.display = "block";
+        else
+            toTopButton.style.display = "none";
+       
+       test.innerHTML = yScrollAxis;
+    };
+ 
  toTopButton.onclick = function()
  {
-    window.scrollBy(0, 100); 
+    window.scrollBy(0, -1 * window.pageYOffset); 
  };
 };
 
