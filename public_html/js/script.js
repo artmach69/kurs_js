cbce6d44bcc2164d4dba9e8084c347f7c131536c
Artur Machowski <artmach@wp.pl>
2018-09-20 20:27:13 +0200

    stoper_zmiana

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,44 +4,68 @@
  * and open the template in the editor.
  */
 
-var timeOutStoper;
-
-
-function stopwatch(uchwytStopera, liczba)
+function Stopwatch(uchwytStopera)
 {
-    uchwytStopera.innerHTML = liczba--;
-    
-    if (liczba < 0 )
-        return;
-    
-    timeOutStoper = setTimeout(function()
-    {
-      stopwatch(uchwytStopera, liczba);  
-    }, 1000);
-    return timeOutStoper;
+  this.uchwytStopera = uchwytStopera;
+  this.poczatkowaWartosc;
+  this.timeOutRef = undefined;
+  this.odpal = function (poczatkowaWartosc)
+  {
+     this.poczatkowaWartosc = poczatkowaWartosc;
+     if (this.timeOutRef)
+         this.zatrzymaj();
+  
+     this.startStoper();
+  };
+  
+  this.startStoper = function()
+  {
+      if (this.poczatkowaWartosc < 0)
+          return;
+      
+      this.uchwytStopera.innerHTML = this.poczatkowaWartosc--;
+      
+      var self = this;
+      
+      this.timeOutRef = setTimeout(function()
+      {
+          self.startStoper();
+      }, 1000);
+  };
+  
+  this.zatrzymaj = function ()
+  {
+      clearTimeOut(this.TimeOutRef);
+  };
+  this.kontynuuj = function ()
+  {
+      this.startStoper();
+  };
 }
 
 window.onload = function()
-{
-    
+{    
     var przyciskOdpalStoper = document.querySelector("#przyciskOdpalStoper"); 
     var przyciskZatrzymajStoper = document.querySelector("#przyciskZatrzymajStoper");
+    var przyciskKontynuujStoper = document.querySelector("#przyciskKontynuujStoper");
     var uchwytStopera = document.querySelector("#uchwytStopera");
     
+    var stoper = new Stopwatch(uchwytStopera);
     
-    przyciskOdpalStoper.onclick = function()
-    {
-        var poczatkowaWartosc = document.querySelector("#poczatkowaWartosc").value;
-        uchwytStopera.innerHTML = poczatkowaWartosc;
-       
-        if (timeOutStoper)
-            clearTimeout(timeOutStoper);
-        
-        timeOutStoper = stopwatch(uchwytStopera, poczatkowaWartosc);
+ przyciskOdpalStoper.onclick = function ()
+ {
+     var poczatkowaWartosc = document.querySelector("#poczatkowaWartosc").value;
+     stoper.odpal(poczatkowaWartosc);
+ };
+ 
+ przyciskZatrzymajStoper.onclick = function ()
+ {
+     stoper.zatrzymaj();
+ };
+ 
+  przyciskKontynuujStoper.onclick = function ()
+ {
+     stoper.kontynuuj();
+ };
 
-    };
-    przyciskZatrzymajStoper.onclick = function ()
-    {
-        clearTimeout(timeOutStoper);
-    }
 };
