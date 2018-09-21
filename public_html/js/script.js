2a364ebfd29e6cf188f1911ff9b8ffb785d7b269
Artur Machowski <artmach@wp.pl>
2018-09-21 13:39:43 +0200

    form_check

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -14,19 +14,24 @@ window.onload = function()
 {
     var poleLiczbowe = document.querySelector("#myForm").poleLiczbowe;
     var poleTekstowe = document.querySelector("#myForm").poleTekstowe;
+    var submitMyForm = document.querySelector("#myForm").submitMyForm;
     
     
     var info = document.querySelector("#info");
     var i = 0;
+    var isEverythingOK = true;
+    
+    
     poleLiczbowe.onkeyup = function(e)
     {
         
         var wpisanyZnak = e.which;
         
-        if (isNumber (yhis.value))
+        if (isNumber (this.value))
         {
         this.style.backgroundColor = "green";
         info.innerHTML = "";
+        isEverythingOK = true;
         
         }
         else
@@ -34,7 +39,13 @@ window.onload = function()
         e.preventDefault();
         this.style.backgroundColor = "red";
         info.innerHTML = "Niepoprawny format, pole przyjmuje tylko liczby";
+        isEverythingOK = false;
         }     
      
     };
+    submitMyForm.onclick = function (e)
+    {
+        if(isEverythingOK)
+        e.preventDefault();
+    }
 };
