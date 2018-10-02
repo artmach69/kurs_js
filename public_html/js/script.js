a4f21a6a38c1fd056f4e294e4c8d6c162c939a0f
artmach69 <artmach@wp.pl>
2018-10-02 14:04:51 +0200

    clock

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,45 +4,34 @@
  * and open the template in the editor.
  */
 
-Date.prototype.getMonthPL = function ()
-{
- var months = ["Styczeń", "Luty", "Marzec", "Kwieceiń", "Maj", "Czerwiec",
-               "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", 
-               "Grudzień"]; 
-           
-    return months[this.getMonth()];
-};
 
-function getDayFromNumber(dayNumber)
-{
- var months = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota",
-               "Niedziela"]; 
-           
-    return months[dayNumber];
-}
 
-function ileCzasuMinelo(from, to)
+function Clock(elementHandler)
 {
-    return  "s:"+(from - to)/1000 + "min:"+(from - to)/1000/60 + "h:"+(from - to)/1000/60/60 + "days:"+(from - to)/1000/60/60/24;
+   this.elementHandler = elementHandler; 
+   this.actualDate = new Date();
+   this.start = function()
+   {
+      this.updateElementHandlerContent();
+       var self = this;
+       setInterval(function(){ self.addSecond();self.updateElementHandlerContent();}, 1000);
+      // this.elementHandler.innerHTML = "a";
+   };
+   this.addSecond = function()
+   {
+       this.actualDate = new Date();
+   };
+   this.updateElementHandlerContent = function()
+   {
+      this.elementHandler.innerHTML = this.actualDate.toLocaleTimeString(); 
+   };
 }
 
 window.onload = function()
 {
    var info = document.getElementById("info");
-   
-   var dzis = new Date();
-   
-  // dzis.setDate(dzis.getDate()+1);
-   
-   var jutro = new Date(dzis.getTime() + 1000*60*60*24);
-   //var jutro = new Date(dzis.getFullYear(), dzis.getMonth(), dzis.getDate() +1);
-   //var jutro = new Date("2018-10-01");
-   //var miesiacPL = getMonthFromNumber(jutro.getMonth());
-   //var dzienPL = getDayFromNumber(jutro.getDay());
-   
-   info.innerHTML = ileCzasuMinelo(jutro, dzis);
-           
-            //dzis.getDate() + "/" + (dzis.getMonth()+1) + "/" + dzis.getFullYear(); 
-   
+   var clock = new Clock(info);
+   clock.start();
+  
    
 };
