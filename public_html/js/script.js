3df73d80d8b1eca96bfa9472d30557544c78a085
artmach69 <artmach@wp.pl>
2018-10-02 11:35:53 +0200

    date_zmiana

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -4,14 +4,14 @@
  * and open the template in the editor.
  */
 
-function getMonthFromNumber(monthNumber)
+Date.prototype.getMonthPL = function ()
 {
  var months = ["Styczeń", "Luty", "Marzec", "Kwieceiń", "Maj", "Czerwiec",
                "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", 
                "Grudzień"]; 
            
-    return months[monthNumber];
-}
+    return months[this.getMonth()];
+};
 
 function getDayFromNumber(dayNumber)
 {
@@ -21,19 +21,26 @@ function getDayFromNumber(dayNumber)
     return months[dayNumber];
 }
 
+function ileCzasuMinelo(from, to)
+{
+    return  "s:"+(from - to)/1000 + "min:"+(from - to)/1000/60 + "h:"+(from - to)/1000/60/60 + "days:"+(from - to)/1000/60/60/24;
+}
+
 window.onload = function()
 {
    var info = document.getElementById("info");
    
-   var dzis = new Date(); 
+   var dzis = new Date();
    
-   //var jutro = new Date(dzis.getTime() + 1000*60*60*24);
+  // dzis.setDate(dzis.getDate()+1);
+   
+   var jutro = new Date(dzis.getTime() + 1000*60*60*24);
    //var jutro = new Date(dzis.getFullYear(), dzis.getMonth(), dzis.getDate() +1);
-   var jutro = new Date("2018-10-01");
-   var miesiacPL = getMonthFromNumber(jutro.getMonth());
-   var dzienPL = getDayFromNumber(jutro.getDay());
+   //var jutro = new Date("2018-10-01");
+   //var miesiacPL = getMonthFromNumber(jutro.getMonth());
+   //var dzienPL = getDayFromNumber(jutro.getDay());
    
-   info.innerHTML = dzienPL + " " + miesiacPL;
+   info.innerHTML = ileCzasuMinelo(jutro, dzis);
            
             //dzis.getDate() + "/" + (dzis.getMonth()+1) + "/" + dzis.getFullYear(); 
    
